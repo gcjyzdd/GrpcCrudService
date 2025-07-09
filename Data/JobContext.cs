95e5eefb02e3af08fa7849b9492df9f077759866
Changjie Guan <changjieguan.test@gmail.com>
2025-07-09 18:15:12 +0200

    Create JobContext DbContext for database operations

---
+++ Data/JobContext.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using JobService.Models;
+
+namespace JobService.Data
+{
+    public class JobContext : DbContext
+    {
+        public JobContext(DbContextOptions<JobContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Job> Jobs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Job>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
+                entity.Property(e => e.Name).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.WorkDir).IsRequired().HasMaxLength(500);
+                entity.Property(e => e.ClusterName).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.CreatedAt).IsRequired();
+            });
+        }
+    }
+}
