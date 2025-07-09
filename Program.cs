9553bdf977b400da6e345e19bdda8ede32304095
Changjie Guan <changjieguan.test@gmail.com>
2025-07-09 21:09:55 +0200

    Remove unused files

--- Program.cs
+++ Program.cs
@@ -14,7 +14,6 @@ builder.Services.AddDbContext<JobContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
 app.MapGrpcService<JobService.Services.JobGrpcService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
