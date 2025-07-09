52f5ee40d10a94b58e9030cf094964cdd7a5ca13
Changjie Guan <changjieguan.test@gmail.com>
2025-07-09 18:16:50 +0200

    Configure services in Program.cs and add database connection string

--- Program.cs
+++ Program.cs
@@ -1,14 +1,21 @@
 using JobService.Services;
+using JobService.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
 
+// Add Entity Framework
+builder.Services.AddDbContext<JobContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
+app.MapGrpcService<JobService.Services.JobService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
