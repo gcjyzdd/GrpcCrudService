f9f12745f969c74b5b714503cfc38e20c1142434
Changjie Guan <changjieguan@gmail.com>
2025-07-20 22:38:40 +0200

    See #201. Use explicit assembly info to set version resource

---
+++ VersionInfo/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Reflection;
+
+[assembly: System.Reflection.AssemblyCompanyAttribute("GrpcCrudService Project")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © GrpcCrudService Project 2025")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+823070f")]
+[assembly: System.Reflection.AssemblyProductAttribute("VersionInfo Utility")]
+[assembly: System.Reflection.AssemblyTitleAttribute("VersionInfo")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.1.0")]
