52e63964eaa9576c8c581fff4feca4b8d10aa984
Changjie Guan <changjieguan.test@gmail.com>
2025-07-22 22:19:19 +0200

    See #203. Set up Autofac container and service registration

--- SimpleGui/App.axaml.cs
+++ SimpleGui/App.axaml.cs
@@ -4,16 +4,38 @@ using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
 using System.Linq;
 using Avalonia.Markup.Xaml;
+using Autofac;
 using SimpleGui.ViewModels;
 using SimpleGui.Views;
+using SimpleGui.Services;
+using Wrappers;
 
 namespace SimpleGui;
 
 public partial class App : Application
 {
+    private IContainer? _container;
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
+        ConfigureServices();
+    }
+
+    private void ConfigureServices()
+    {
+        var builder = new ContainerBuilder();
+
+        // Register wrappers
+        builder.RegisterType<FileWrapper>().As<IFileWrapper>().SingleInstance();
+
+        // Register services
+        builder.RegisterType<ConfigurationService>().As<IConfigurationService>().SingleInstance();
+
+        // Register ViewModels
+        builder.RegisterType<MainWindowViewModel>().AsSelf();
+
+        _container = builder.Build();
     }
 
     public override void OnFrameworkInitializationCompleted()
@@ -23,9 +45,11 @@ public partial class App : Application
             // Avoid duplicate validations from both Avalonia and the CommunityToolkit.
             // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
             DisableAvaloniaDataAnnotationValidation();
+            
+            var viewModel = _container?.Resolve<MainWindowViewModel>() ?? new MainWindowViewModel();
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainWindowViewModel(),
+                DataContext = viewModel,
             };
         }
 
